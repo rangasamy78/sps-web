c813a66c0916d87fcb8775005e5f86e7b68b5fe8
Rangasamy <rangasamy.ultrastone@gmail.com>
2024-09-02 15:54:50 +0530

    #RAN - Laravel Debugger Install Package

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class
     ])->toArray(),
 
     /*
@@ -185,6 +186,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'Debugbar'   => Barryvdh\Debugbar\Facade::class,
     ])->toArray(),
 
 ];
