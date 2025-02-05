09d97fe5ca7d9acdbe66f294ba09d50e03eab37e
Rangasamy <rangasamy.ultrastone@gmail.com>
2025-02-05 12:49:19 +0530

    #RAN - Home blade file changes

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -67,10 +67,10 @@ class Service extends Model
     {
         return $this->hasMany(VisitService::class, 'service_id');
     }
-
     public function unit_measures_so()
     {
         return $this->belongsTo(UnitMeasure::class, 'unit_of_measure_id');
+    }
     public function sample_order_service()
     {
         return $this->hasMany(SampleOrderService::class, 'service_id');
@@ -79,15 +79,14 @@ class Service extends Model
     {
         return $this->hasMany(QuoteService::class, 'service_id');
     }
-    function linked_account_gl()
+    public function linked_account_gl()
     {
         return $this->belongsTo(Account::class, 'gl_sales_account_id');
     }
-    function linked_account()
+    public function linked_account()
     {
         return $this->belongsTo(Account::class, 'gl_cost_of_sales_account_id');
     }
-
     public function services()
     {
         return $this->hasMany(OtherCharges::class, 'service_id');
